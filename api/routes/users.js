8204211a95fb51ad8e6e11bdcb88129ff4754e2f
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-05-11 21:30:55 -0700

    token setting

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,8 +5,13 @@ import {
   getUser,
   getUsers,
 } from "../controllers/user.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+  res.send("hello user, you are logged in");
+});
 
 //UPDATE
 router.put("/:id", updateUser);
@@ -18,6 +23,6 @@ router.delete("/:id", deleteUser);
 router.get("/:id", getUser);
 
 //GET ALL
-router.get("/",getUsers);
+router.get("/", getUsers);
 
 export default router;
