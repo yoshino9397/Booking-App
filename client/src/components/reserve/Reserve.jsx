fd8959a569e5398c3e8cdad84c6f6f03c28a8a5c
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-05-17 23:20:32 -0700

    CSS redesign

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -14,13 +14,21 @@ const Reserve = ({ setOpen, hotelId }) => {
     const start = new Date(startDate);
     const end = new Date(endDate);
     const date = new Date(start.getTime());
-    
     const dates = [];
+
     while (date <= end) {
       dates.push(new Date(date).getTime());
       date.setDate(date.getDate() + 1);
     }
     return dates;
+  };
+
+  const alldates = getDatesInRange(dates[0].startDate, dates[0].endDate);
+  const isAvailable = (roomNumber) => {
+    const isFound = roomNumber.unavailableDates.some((date) =>
+      alldates.includes(new Date(date).getTime())
+    );
+    return !isFound;
   };
 
   const handleSelect = (e) => {
