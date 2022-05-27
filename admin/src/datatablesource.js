81980f8bf5b3a2b76cab2118d46b0ec5bec7add3
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-05-27 01:21:46 -0700

    added columns

--- admin/src/datatablesource.js
+++ admin/src/datatablesource.js
@@ -7,7 +7,14 @@ export const userColumns = [
     renderCell: (params) => {
       return (
         <div className="cellWithImg">
-          <img className="cellImg" src={params.row.img} alt="avatar" />
+          <img
+            className="cellImg"
+            src={
+              params.row.img ||
+              "https://live.staticflickr.com/65535/51978404588_b3fe57fa1b.jpg"
+            }
+            alt="avatar"
+          />
           {params.row.username}
         </div>
       );
@@ -31,6 +38,54 @@ export const userColumns = [
   {
     field: "phone",
     headerName: "Phone",
+    width: 100,
+  },
+];
+
+export const hotelColumns = [
+  { field: "_id", headerName: "ID", width: 250 },
+  {
+    field: "name",
+    headerName: "Name",
+    width: 150,
+  },
+  {
+    field: "type",
+    headerName: "Type",
+    width: 100,
+  },
+  {
+    field: "title",
+    headerName: "Title",
+    width: 230,
+  },
+  {
+    field: "city",
+    headerName: "City",
+    width: 100,
+  },
+];
+
+export const roomColumns = [
+  { field: "_id", headerName: "ID", width: 70 },
+  {
+    field: "title",
+    headerName: "Title",
+    width: 230,
+  },
+  {
+    field: "desc",
+    headerName: "Description",
+    width: 200,
+  },
+  {
+    field: "price",
+    headerName: "Price",
+    width: 100,
+  },
+  {
+    field: "maxPeople",
+    headerName: "Max People",
     width: 100,
   },
 ];
